2af1e9ee2f587e7ad40b9ddb3b505759445e8979
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2023-11-03 20:41:34 +0530

    blog app

---
+++ src/app/Components/landing/NavBar.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import {AiFillInstagram,AiFillLinkedin} from 'react-icons/ai';
+import {BsTwitter} from 'react-icons/bs';
+import {BiUserCircle,BiSearchAlt} from 'react-icons/bi';
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <nav className='w-full h-full font-josefinSans'>
+      <div className='mx-5 py-2 lg:flex lg:justify-center lg:items-center lg:gap-5'>
+        <div className='w-full flex-[3] py-3 px-1'>
+          <div className='flex justify-center items-center gap-10 text-2xl'>
+            <Link href={'#'} target='_blank'>
+              <AiFillInstagram/>
+            </Link>
+            <Link href={'#'} target='_blank'>
+              <BsTwitter/>
+            </Link>
+            <Link href={'#'} target='_blank'>
+              <AiFillLinkedin/>
+            </Link>
+          </div>
+        </div>
+        <div className='w-full flex-[6] py-3 px-1'>
+          <ul className='flex justify-center items-center gap-5 lg:gap-10 font-semibold flex-wrap'>
+            <li>
+              <Link href={'#'}>
+               HOME
+              </Link>
+            </li>
+            <li>
+              <Link href={'#'}>
+               ABOUT
+              </Link>
+            </li>
+            <li>
+              <Link href={'#'}>
+               CONTACT
+              </Link>
+            </li>
+            <li>
+              <Link href={'#'}>
+               WRITE
+              </Link>
+            </li>
+            <li>
+              <Link href={'#'}>
+               LOGOUT
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className='w-full flex-[3] py-3 px-1 hidden lg:block'>
+          <div className='flex lg:justify-center lg:items-center gap-3'>
+            <BiUserCircle className='text-4xl hidden lg:block'/>
+            <div className='flex justify-center items-center border gap-2 py-1 px-2 rounded-md border-black'>
+              <label htmlFor="search">
+                <BiSearchAlt/>
+              </label>
+              <input type="text" name="search" id="search" className='bg-transparent outline-none' placeholder='search'/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily:{
+        josefinSans:['Josefin Sans', 'sans-serif'],
+        youngSerif:['Young Serif', 'serif'],
+        lora:['Lora', 'serif'],
+        verelaRound:['Varela Round', 'sans-serif']
+      }
     },
   },
   plugins: [],
