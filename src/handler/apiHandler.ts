f9066e15af2ea2b00a79066e0391ecdc24f2d5fd
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2023-11-12 22:36:54 +0530

    add

--- src/handler/apiHandler.ts
+++ src/handler/apiHandler.ts
@@ -77,7 +77,7 @@ export const checkEnvironment = () => {
     let base_url =
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000"
-        : "https://example.com"; // https://v2ds.netlify.app
+        : process.env.NEXTAUTH_URL as string; // https://v2ds.netlify.app
   
     return base_url;
   };
