3136ba229dad917f5d4ab9bfd2010ae1657cc895
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2023-11-13 10:12:44 +0530

    add

--- src/app/Components/components/Post.tsx
+++ src/app/Components/components/Post.tsx
@@ -18,7 +18,7 @@ export default function Post({id,title,img,description,createdAt}:PostsType) {
       </div>
       <section className="space-y-1">
         <h2 className="text-xl lg:text-2xl font-josefinSans cursor-pointer hover:underline">
-          <Link href={`/posts/${id.toString()}`}>
+          <Link href={`/posts/${id}`}>
             {title}
           </Link>
         </h2>
